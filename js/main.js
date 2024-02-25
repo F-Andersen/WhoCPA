aaf5dac916ab161d8a0640e3a2126aef8d40610b
F_Andersen <masxpros@gmail.com>
2024-02-25 23:36:29 +0200

    .. section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+const changeContentButton = document.getElementById('changeContentButton');
+
+changeContentButton.addEventListener('click', function() {
+  // Знаходимо всі елементи з класом 'hidden' і змінюємо їх клас на 'visible'
+  const hiddenSections = document.querySelectorAll('.hidden');
+  hiddenSections.forEach(section => {
+    section.classList.remove('hidden');
+    section.classList.add('visible');
+  });
+});
+
+// Знаходимо всі секції і приховуємо їх, крім першої
+const allSections = document.querySelectorAll('section');
+allSections.forEach((section, index) => {
+  if (index !== 0) {
+    section.classList.add('hidden');
+  }
+});
+
 // function openGift() {
 //     var lid = document.querySelector('.lid');
 //     lid.classList.add('open'); // Додаємо клас, що спрацює анімацію
