f0876cdf331d0a7f15780fdedd13003fd3799487
F_Andersen <masxpros@gmail.com>
2024-02-26 16:14:40 +0200

    new and successful feature section

--- js/main.js
+++ js/main.js
@@ -17,9 +17,6 @@
 //   }
 // });
 
-
-
-
 // const changeContentButton = document.getElementById('changeContentButton');
 // const sections = document.querySelectorAll('section');
 // let currentIndex = 0;
@@ -27,7 +24,7 @@
 // changeContentButton.addEventListener('click', function() {
 //   // Приховуємо поточний елемент
 //   sections[currentIndex].classList.add('hidden');
-  
+
 //   // Збільшуємо індекс, але забезпечуємо зациклення
 //   currentIndex = (currentIndex + 1) % sections.length;
 
@@ -66,58 +63,38 @@
 // // changeSectionButton.addEventListener('click', changeSection);
 // changeContentButton.addEventListener('click', changeSection);
 
-
 // // Приховуємо всі секції, окрім першої
 // for (let i = 1; i < sections.length; i++) {
 //   sections[i].classList.add('hidden');
 // }
 
-
-
 // робе
 
-
 // Отримуємо всі кнопки "Змінити вміст" у всіх секціях
-const changeSectionButtons = document.querySelectorAll('section button#changeSectionButton');
+// const changeSectionButtons = document.querySelectorAll('section button#changeSectionButton');
 
-// Функція для зміни секції при натисканні кнопки
-function changeSection(event) {
-  const currentSection = event.target.closest('section'); // Отримуємо батьківську секцію кнопки, на яку натиснуто
-  currentSection.classList.add('hidden'); // Приховуємо поточну секцію
-  const nextSection = currentSection.nextElementSibling; // Отримуємо наступну секцію
-  if (nextSection) {
-    nextSection.classList.remove('hidden'); // Показуємо наступну секцію, якщо вона існує
-  } else {
-    sections[0].classList.remove('hidden'); // Якщо немає наступної секції, показуємо першу
-  }
-}
+// // Функція для зміни секції при натисканні кнопки
+// function changeSection(event) {
+//   const currentSection = event.target.closest('section'); // Отримуємо батьківську секцію кнопки, на яку натиснуто
+//   currentSection.classList.add('hidden'); // Приховуємо поточну секцію
+//   const nextSection = currentSection.nextElementSibling; // Отримуємо наступну секцію
+//   if (nextSection) {
+//     nextSection.classList.remove('hidden'); // Показуємо наступну секцію, якщо вона існує
+//   } else {
+//     sections[0].classList.remove('hidden'); // Якщо немає наступної секції, показуємо першу
+//   }
+// }
 
-// Навішуємо обробник події на кожну кнопку "Змінити вміст"
-changeSectionButtons.forEach(button => {
-  button.addEventListener('click', changeSection);
-});
+// // Навішуємо обробник події на кожну кнопку "Змінити вміст"
+// changeSectionButtons.forEach(button => {
+//   button.addEventListener('click', changeSection);
+// });
 
-// Приховуємо всі секції, окрім першої
-const sections = document.querySelectorAll('section');
-for (let i = 1; i < sections.length; i++) {
-  sections[i].classList.add('hidden');
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// // Приховуємо всі секції, окрім першої
+// const sections = document.querySelectorAll('section');
+// for (let i = 1; i < sections.length; i++) {
+//   sections[i].classList.add('hidden');
+// }
 
 // function openGift() {
 //     var lid = document.querySelector('.lid');
@@ -131,3 +108,81 @@ for (let i = 1; i < sections.length; i++) {
 // boxText.addEventListener('click', function() {
 //     openGift(); // Викликаємо функцію відкриття подарунка при кліку на текст
 // });
+
+// const changeSectionButtons = document.querySelectorAll('section button#changeSectionButton');
+
+// function changeSection(event) {
+//   const currentSection = event.target.closest('section');
+//   const nextSection = currentSection.nextElementSibling || sections[0];
+//   nextSection.classList.remove('hidden');
+//   currentSection.classList.add('hidden');
+// }
+
+// changeSectionButtons.forEach(button => {
+//   button.addEventListener('click', changeSection);
+// });
+
+// const sections = document.querySelectorAll('section');
+// for (let i = 1; i < sections.length; i++) {
+//   sections[i].classList.add('hidden');
+// }
+
+// function changeSectionManually() {
+//   const visibleSection = document.querySelector('section:not(.hidden)');
+//   if (!visibleSection) return;
+
+//   const nextSection = visibleSection.nextElementSibling || sections[0];
+//   if (visibleSection.id === 'content2') {
+//     nextSection.classList.remove('hidden');
+//     visibleSection.classList.add('hidden');
+//   }
+
+// }
+
+// if (document.getElementById('content2')) {
+//   setInterval(changeSectionManually, 1000);
+// }
+
+// РОБЕ !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// РОБЕ НО НА КОСТИЛЯХ  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+const changeSectionButtons = document.querySelectorAll('section button[id^="changeSectionButton"]');
+const changeSectionButtons_1 = document.getElementById("changeSectionButton_1");
+const changeSectionButtons_2 = document.getElementById("changeSectionButton_2");
+
+function changeSection(event) {
+  const currentSection = event.target.closest("section");
+  const nextSection = currentSection.nextElementSibling || sections[0];
+
+  if (event.target === changeSectionButtons_1) {
+    nextSection.classList.remove("hidden");
+    currentSection.classList.add("hidden");
+    setTimeout(() => {
+      const nextNextSection = nextSection.nextElementSibling || sections[0];
+      nextNextSection.classList.remove("hidden");
+      nextSection.classList.add("hidden");
+    }, 3000);
+  } else if (event.target === changeSectionButtons_2) {
+    const nextNextSection = nextSection.nextElementSibling || sections[0];
+    nextSection.classList.remove("hidden");
+    currentSection.classList.add("hidden");
+    setTimeout(() => {
+      nextNextSection.classList.remove("hidden");
+      nextSection.classList.add("hidden");
+    }, 3000);
+  } else {
+    nextSection.classList.remove("hidden");
+    currentSection.classList.add("hidden");
+  }
+}
+
+changeSectionButtons.forEach((button) => {
+  button.addEventListener("click", changeSection);
+});
+
+const sections = document.querySelectorAll("section");
+for (let i = 1; i < sections.length; i++) {
+  sections[i].classList.add("hidden");
+}
