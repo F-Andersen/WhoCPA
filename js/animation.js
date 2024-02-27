5bf8a284f137b22c3bbf2358113d6dc7aa2325e9
F_Andersen <masxpros@gmail.com>
2024-02-27 03:20:00 +0200

    TEXT

---
+++ js/animation.js
@@ -0,0 +1,34 @@
+// анімація
+
+function openGift() {
+    var lid = document.querySelector('.lid');
+    lid.classList.add('open'); // Додаємо клас, що спрацює анімацію
+}
+
+// Отримуємо посилання на елемент з текстом на коробці
+var boxText = document.querySelector('.box-text');
+
+// Додаємо обробник події для тексту на коробці
+boxText.addEventListener('click', function() {
+    openGift(); // Викликаємо функцію відкриття подарунка при кліку на текст
+});
+
+
+
+// поява тексту
+
+// Отримуємо всі елементи рядків тексту
+const textLines = document.querySelectorAll('#textContainer p');
+
+// Функція для поступового з'явлення рядків тексту
+function showText() {
+  textLines.forEach((line, index) => {
+    setTimeout(() => {
+      line.classList.remove('hidden__text');
+    }, index * 1000); // Затримка для кожного рядка (в мілісекундах)
+  });
+}
+
+// Викликаємо функцію для поступового з'явлення рядків тексту
+showText();
+
