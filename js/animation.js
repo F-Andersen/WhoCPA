b0b71fa73b1538cec6645a82ee7916ee97e09a98
F_Andersen <masxpros@gmail.com>
2024-03-01 00:28:54 +0200

    modal

--- js/animation.js
+++ js/animation.js
@@ -1,19 +1,33 @@
 // анімація
 
-function openGift() {
-    var lid = document.querySelector('.lid');
-    lid.classList.add('open'); // Додаємо клас, що спрацює анімацію
+// function openGift() {
+//     var lid = document.querySelector('.lid');
+//     lid.classList.add('open'); // Додаємо клас, що спрацює анімацію
+// }
+
+// // Отримуємо посилання на елемент з текстом на коробці
+// var boxText = document.querySelector('.box-text');
+
+// // Додаємо обробник події для тексту на коробці
+// boxText.addEventListener('click', function() {
+//     openGift(); // Викликаємо функцію відкриття подарунка при кліку на текст
+// });
+
+
+
+function openGift(event) {
+  var currentGift = event.currentTarget; // Поточна коробка, на яку клікнули
+  var lid = currentGift.querySelector('.lid');
+  lid.classList.add('open'); // Додаємо клас, що спрацює анімацію
 }
 
-// Отримуємо посилання на елемент з текстом на коробці
-var boxText = document.querySelector('.box-text');
+// Отримуємо посилання на всі елементи з класом box-text
+var boxTexts = document.querySelectorAll('.box-text');
 
-// Додаємо обробник події для тексту на коробці
-boxText.addEventListener('click', function() {
-    openGift(); // Викликаємо функцію відкриття подарунка при кліку на текст
+// Додаємо обробник події для кожного елементу з класом box-text
+boxTexts.forEach(function(boxText) {
+  boxText.addEventListener('click', openGift); // Викликаємо функцію відкриття подарунка при кліку на текст
 });
-
-
 
 // поява тексту
 
@@ -32,3 +46,32 @@ function showText() {
 // Викликаємо функцію для поступового з'явлення рядків тексту
 showText();
 
+  //  modal
+
+  // Отримання елементів DOM
+// Отримання елементів DOM
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("openModal");
+var span = document.getElementsByClassName("close")[0];
+var okBtn = document.getElementById("modalOkBtn");
+
+// Коли користувач клікає на кнопку, відкрити модальне вікно
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// Коли користувач клікає на (x) або кнопку ОК, закрити модальне вікно
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+okBtn.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Коли користувач клікає поза модальним вікном, закрити його
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
